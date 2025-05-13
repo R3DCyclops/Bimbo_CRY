dd781a5e6f480a6e483faa03495c5ac4763c6b39
R3DCyclops <58948389+R3DCyclops@users.noreply.github.com>
2025-05-13 15:05:50 +0300

    Add files via upload

--- compile2.py
+++ compile2.py
@@ -4,13 +4,16 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-script_path = os.path.join(current_dir, "BimboCRY.py")
+script_path = os.path.join(current_dir, "cry.py")
 
 output_dir = os.path.join(current_dir, "dist")
 
 icon_path = os.path.join(current_dir, "ico.ico")
 
-image_path = os.path.join(current_dir, "Group2.png")
+group2_path = os.path.join(current_dir, "Group2.png")
+group3_path = os.path.join(current_dir, "Group3.png")
+
+style_path = os.path.join(current_dir, "style.qss")
 
 command = [
     'pyinstaller', 
@@ -19,10 +22,15 @@ command = [
     '--hidden-import=PySide6',
     '--hidden-import=pyperclip',
     '--hidden-import=random',
+    '--hidden-import=collections.defaultdict',
+    '--hidden-import=PIL',
+    '--hidden-import=PIL.Image',
     '--icon', icon_path,
     '--distpath', output_dir,
+    '--add-data', f'{group2_path};.',
+    '--add-data', f'{group3_path};.',
     '--add-data', f'{icon_path};.',
-    '--add-data', f'{image_path};.',
+    '--add-data', f'{style_path};.',
     script_path
 ]
 
